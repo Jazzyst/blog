9c2188e9582ec572bb8b96b8d7b1928a82dbeb88
a <a@iMac-a.local>
2017-09-18 10:09:31 +0300

    upd

--- routes/web.php
+++ routes/web.php
@@ -17,25 +17,54 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Auth::routes();
 Route::get('/home', 'HomeController@index');
 
-Route::get('/posts','PostController@index')->name('Главная');
-Route::get('/posts/show/{vacancy}','PostController@show');
-Route::get('/posts/create','PostController@create');
-Route::post('/posts/post','PostController@store');
 
 
-Route::get('/profile/', 'ProfileController@index');
+Route::get('/dev', function (\App\User $user)
+{
+    phpinfo();
+});
 
-Route::get('/category', 'CategoryController@index');
-Route::get('/category/{category}', 'CategoryController@show');
-Route::get('/add/category', 'CategoryController@create');
-Route::post('add/category/', 'CategoryController@store');
+Route::group( ['middleware' => ['auth']], function() {
 
-Route::post('/comments/add/{post}','CommentsController@store');
+    // Post Routes
+    Route::get('/posts','PostController@index')->name('posts.index');
+    Route::get('/posts/show/{post}','PostController@show')->name('post.show');
+    Route::get('/posts/create','PostController@create')->name('post.create');
+    Route::post('/posts/post','PostController@store')->name('post.store');
 
-Route::get('/profile/show/{id}', 'ProfileController@show');
 
-Route::get('/contact', 'AboutController@index');
-Route::post('/contact/send', 'AboutController@store');
+    // Category Routes
+    Route::get('/category', 'CategoryController@index')->name('category.index');
+    Route::get('/category/{category}', 'CategoryController@show')->name('category.show');
+    Route::get('/add/category', 'CategoryController@create')->name('category.create');
+    Route::post('add/category/', 'CategoryController@store')->name('category.store');
+
+    // Profile Routes
+    Route::get('/profile/', 'ProfileController@index')->name('profile.index');
+    Route::get('/profile/show/{id}', 'ProfileController@show')->name('profile.show');
+
+    // Comments Routes
+    Route::post('/comments/add/{post}','CommentsController@store');
+
+    // About Routes
+    Route::get('/contact', 'AboutController@index');
+    Route::post('/contact/send', 'AboutController@store');
+
+    Route::prefix('admin')->group(function () {
+
+        // Admin User Manage Routes
+        Route::get('/index', 'AdminController@index');
+        Route::get('/users', 'AdminController@users');
+        Route::get('/users/{user}', 'AdminController@show');
+
+        // Admin Post Manage Routes
+        Route::get('/posts', 'AdminController@posts');
+        Route::get('/posts/{post}', 'AdminController@showPost');
+
+        // Admin Comments Manage Routes
+        Route::get('/comments', 'AdminController@comments');
+        Route::get('/comments/{comment}', 'AdminController@showComment');
+    });
+});

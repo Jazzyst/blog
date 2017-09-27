4891546f7d271e9e054d693451756729f065c19e
@andriy <andriy.zhuravlov@gmail.com>
2017-09-27 12:22:42 +0300

    add the logic to create.blade.php

--- resources/views/comments/create.blade.php
+++ resources/views/comments/create.blade.php
@@ -4,12 +4,36 @@
     <div class="card">
         <div class="card-block">
             <form>
-                <div class="form-group">
+
+                @auth
+                    <div class="form-group">
                     <textarea name="body" placeholder="Add coment here." class="form-control">
 
                     </textarea><br>
-                    <button type="submit" class="btn btn-primary">add comment</button>
-                </div>
+                        <button type="submit" class="btn btn-primary">add comment</button>
+                    </div>
+                @endauth
+
+                @guest
+                    <div class="form-group">
+                    <textarea name="body" placeholder="Add coment here." class="form-control">
+
+                    </textarea><br>
+                        <button type="submit" class="btn btn-primary">add comment</button>
+                        <div class="unregistred-user">
+                            <span class="warning-message">To add message login :</span>
+                            <div class="social-links">
+                                <a href="/login/vk" class="vk"></a>
+                                <a href="/login/fb" class="fb"></a>
+                                <a href="/login/twitter" class="twitt"></a>
+                                <a href="/login/gmail" class="goo"></a>
+                                <a href="/login/ok" class="ok"></a>
+                            </div>
+                        </div>
+
+                    </div>
+                @endguest
+
             </form>
         </div>
     </div>
